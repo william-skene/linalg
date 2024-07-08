2e80a8c438e119ef30267eeb0a7340eb2f74372c
William Skene <william.skene@outlook.com>
2024-07-08 17:37:28 +1000

    Simple documentation and formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use std::cmp::max;
+#![crate_name = "linalg"]
+
+use std::cmp::{max, PartialEq};
 use std::fmt::{self, Display};
 use std::ops::{Add, Index, IndexMut, Mul, MulAssign};
+
 #[derive(Debug)]
+/// A basic matrix representation
 pub struct Matrix {
     rows: usize,
     cols: usize,
@@ -9,6 +13,17 @@ pub struct Matrix {
 }
 
 impl Matrix {
+    /// Creates a new matrix filled with a scalar value.
+    ///
+    /// # Parameters
+    ///
+    /// - `n_rows`: Number of rows in the matrix.
+    /// - `n_cols`: Number of columns in the matrix.
+    /// - `val`: Scalar value to fill the matrix.
+    ///
+    /// # Returns
+    ///
+    /// A new `Matrix` with dimensions `n_rows` x `n_cols` filled with `val`.
     pub fn from_scalar(n_rows: usize, n_cols: usize, val: f64) -> Self {
         Matrix {
             rows: n_rows,
@@ -17,6 +32,17 @@ impl Matrix {
         }
     }
 
+    /// Creates a new matrix from a 2D vector of floating-point numbers.
+    ///
+    /// # Parameters
+    ///
+    /// - `n_rows`: Number of rows in the matrix.
+    /// - `n_cols`: Number of columns in the matrix.
+    /// - `data`: 2D vector containing matrix elements.
+    ///
+    /// # Returns
+    ///
+    /// A Result containing either the created `Matrix` or an error message if dimensions are inconsistent.
     pub fn from_2d_vec(n_rows: usize, n_cols: usize, data: Vec<Vec<f64>>) -> Result<Self, String> {
         let mut data_formatted = Vec::<f64>::with_capacity(n_rows * n_cols);
         if data.len() != n_rows {
@@ -37,6 +63,15 @@ impl Matrix {
         })
     }
 
+    /// Creates an identity matrix of a given size.
+    ///
+    /// # Parameters
+    ///
+    /// - `size`: Size of the square identity matrix (size x size).
+    ///
+    /// # Returns
+    ///
+    /// An identity matrix of dimensions `size` x `size`.
     pub fn identity(size: usize) -> Self {
         let mut data = vec![0.0; size * size];
         for i in 0..size {
@@ -45,15 +80,25 @@ impl Matrix {
         Matrix {
             rows: size,
             cols: size,
-            data: data,
+            data,
         }
     }
 
+    /// Returns the shape of the matrix.
+    ///
+    /// # Returns
+    ///
+    /// A tuple representing the matrix shape: (rows, cols)
     pub fn shape(&self) -> (usize, usize) {
         // Return the shape of the matrix in the form (rows, cols)
         (self.rows, self.cols)
     }
 
+    /// Transposes the matrix.
+    ///
+    /// # Returns
+    ///
+    /// A new `Matrix` which is the transpose of the current matrix.
     pub fn transpose(self) -> Self {
         let mut ret = Matrix {
             rows: self.cols,
@@ -68,18 +113,33 @@ impl Matrix {
         ret
     }
 
-    pub fn pow(self, pow: usize) -> Self {
+    /// Raises a square matrix to a given power
+    ///
+    /// # Parameters
+    ///
+    /// - `pow`: The exponent to which the matrix is raised.
+    ///
+    /// # Returns
+    ///
+    /// A new `Matrix` which is `self` raised to the power of `pow`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the matrix is not square (`self.rows != self.cols`).
+    pub fn pow(&self, pow: i64) -> Self {
         if self.rows != self.cols {
             panic!("Can only raise square matrices to a power.");
         }
         if pow == 0 {
             return Matrix::identity(self.rows);
+        } else if pow < 0 {
+            panic!("Can only raise matrices to a positive power.");
         }
 
         Self::pow_helper(self.clone(), pow)
     }
 
-    fn pow_helper(mat: Self, pow: usize) -> Self {
+    fn pow_helper(mat: Self, pow: i64) -> Self {
         if pow == 0 {
             return Self::identity(mat.rows);
         } else if pow % 2 == 0 {
@@ -90,11 +150,66 @@ impl Matrix {
     }
 }
 
+impl Index<(usize, usize)> for Matrix {
+    type Output = f64;
+    fn index(&self, (i, j): (usize, usize)) -> &f64 {
+        if i < self.rows && j < self.cols {
+            return &self.data[i * self.cols + j];
+        } else {
+            panic!(
+                "index out of bounds: the shape is ({}, {}) but the index is ({}, {}).",
+                self.rows, self.cols, i, j
+            )
+        }
+    }
+}
+
+impl IndexMut<(usize, usize)> for Matrix {
+    fn index_mut(&mut self, (i, j): (usize, usize)) -> &mut f64 {
+        if i < self.rows && j < self.cols {
+            return &mut self.data[i * self.cols + j];
+        } else {
+            panic!(
+                "index out of bounds: the shape is ({}, {}) but the index is ({}, {}).",
+                self.rows, self.cols, i, j
+            )
+        }
+    }
+}
+
+impl Clone for Matrix {
+    fn clone(&self) -> Self {
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: self.data.clone(),
+        }
+    }
+}
+
+impl PartialEq for Matrix {
+    fn eq(&self, rhs: &Matrix) -> bool {
+        if self.shape() != rhs.shape() {
+            return false;
+        }
+        let (rows, cols) = self.shape();
+        for i in 0..rows {
+            for j in 0..cols {
+                if self[(i, j)] != rhs[(i, j)] {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
 impl Add for Matrix {
     type Output = Matrix;
     fn add(self, other: Matrix) -> Self::Output {
         if self.rows != other.rows || self.cols != other.cols {
-            panic!("Matrices of different shapes cannot be added together. Left({}, {}), Right({}, {})", self.rows, self.cols, other.rows, other.cols);
+            panic!("Matrices of different shapes cannot be added together. Left({}, {}), Right({}, {})", 
+                   self.rows, self.cols, other.rows, other.cols);
         } else {
             return Matrix {
                 rows: self.rows,
@@ -136,9 +251,9 @@ impl Mul for Matrix {
         out
     }
 }
+
 impl MulAssign for Matrix {
     fn mul_assign(&mut self, rhs: Matrix) {
-        // Check that dims are correct
         if self.cols != rhs.rows {
             panic!(
                 "LHS cols must be same as RHS rows to multiply. LHS: ({},{}), RHS: ({}, {})",
@@ -160,6 +275,7 @@ impl MulAssign for Matrix {
         *self = out;
     }
 }
+
 // Scalar Multiplication
 impl Mul<Matrix> for f64 {
     type Output = Matrix;
@@ -222,42 +338,5 @@ impl Display for Matrix {
         s.push('x');
         s.push_str(&self.cols.to_string());
         write!(f, "{}", s)
-    }
-}
-
-impl Index<(usize, usize)> for Matrix {
-    type Output = f64;
-    fn index(&self, (i, j): (usize, usize)) -> &f64 {
-        if i < self.rows && j < self.cols {
-            return &self.data[i * self.cols + j];
-        } else {
-            panic!(
-                "index out of bounds: the shape is ({}, {}) but the index is ({}, {}).",
-                self.rows, self.cols, i, j
-            )
-        }
-    }
-}
-
-impl IndexMut<(usize, usize)> for Matrix {
-    fn index_mut(&mut self, (i, j): (usize, usize)) -> &mut f64 {
-        if i < self.rows && j < self.cols {
-            return &mut self.data[i * self.cols + j];
-        } else {
-            panic!(
-                "index out of bounds: the shape is ({}, {}) but the index is ({}, {}).",
-                self.rows, self.cols, i, j
-            )
-        }
-    }
-}
-
-impl Clone for Matrix {
-    fn clone(&self) -> Self {
-        Matrix {
-            rows: self.rows,
-            cols: self.cols,
-            data: self.data.clone(),
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use linalg::Matrix;
 fn main() {
     let mat1 = Matrix::from_2d_vec(2, 3, vec![vec![1000., 0., 1.], vec![0., 3., 5.]]).unwrap();
     let mat2 = Matrix::from_2d_vec(2, 2, vec![vec![11., 3.], vec![7., 11.]]).unwrap();
-    println!("{mat1}");
-    println!("{mat2}");
-    println!("{}", mat2.clone() + mat2.clone());
-    println!("{}", 15.0 * mat1.clone());
-    println!("{}", (mat2.clone() * mat1.clone()).transpose());
-    println!("{}", mat1.clone().transpose() * mat2.clone().transpose());
-    println!("{}", mat2.pow(5));
+    println!("{mat1}\n");
+    println!("{mat2}\n");
+    println!("{}\n", mat2.clone() + mat2.clone());
+    println!("{}\n", 15.0 * mat1.clone());
+    println!("{}\n", (mat2.clone() * mat1.clone()).transpose());
+    println!("{}\n", mat1.clone().transpose() * mat2.clone().transpose());
+    println!("{}\n", mat2.pow(5));
 }
